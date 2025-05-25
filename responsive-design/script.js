e280ca4be8388f864ce3208c72ba4aef1c512a1e
Shamim Reja <137642627+shamim-reja23@users.noreply.github.com>
2025-05-25 20:12:17 +0530

    Delete responsive-design directory

--- responsive-design/script.js
+++
@@ -1,7 +0,0 @@
-let menu = document.querySelector('#menu-icon');
-let navlist = document.querySelector('.nav-list');
-
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navlist.classList.toggle('open')
-}
